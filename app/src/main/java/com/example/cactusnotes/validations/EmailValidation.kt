64b6399e3b8846ba1fe82473c5e2671a8f1cbf43
Cem FINDIKLI <cemfindikli93@gmail.com>
2021-03-03 11:22:09 +0300

    Email validation bug fix.

--- app/src/main/java/com/example/cactusnotes/validations/EmailValidation.kt
+++ app/src/main/java/com/example/cactusnotes/validations/EmailValidation.kt
@@ -3,12 +3,14 @@ package com.example.cactusnotes.validations
 import android.util.Patterns
 import com.example.cactusnotes.binding
 
+private fun String.emailValid() = contains("@") && contains(".")
+
 fun validateEmail(): Boolean {
     val emailInput: String = binding.emailEditText.editText?.text.toString().trim()
     return if (emailInput.isEmpty()) {
         binding.emailEditText.error = "Email is required"
         false
-    } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+    } else if (!emailInput.emailValid()) {
         binding.emailEditText.error = "Email is invalid."
         false
     } else if (emailInput.length < 5 || emailInput.length > 50) {
