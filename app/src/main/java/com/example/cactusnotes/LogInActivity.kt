bbeb864c6c096dca4d9029e0e736264f20078a11
Cem FINDIKLI <cemfindikli93@gmail.com>
2021-03-19 16:55:41 +0300

    bug fix

--- app/src/main/java/com/example/cactusnotes/LogInActivity.kt
+++ app/src/main/java/com/example/cactusnotes/LogInActivity.kt
@@ -2,6 +2,7 @@ package com.example.cactusnotes
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cactusnotes.databinding.ActivityLogInBinding
