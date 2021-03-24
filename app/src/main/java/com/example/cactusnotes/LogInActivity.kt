5bad63f86add8468d8b84615ad84a5f89a00a9f6
Cem FINDIKLI <cemfindikli93@gmail.com>
2021-03-24 12:25:51 +0300

    Some code iterations

--- app/src/main/java/com/example/cactusnotes/LogInActivity.kt
+++ app/src/main/java/com/example/cactusnotes/LogInActivity.kt
@@ -2,7 +2,6 @@ package com.example.cactusnotes
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cactusnotes.databinding.ActivityLogInBinding
