b32623241a70c783829d792ddda9286297b4dbdd
Ahmet Safa Orhan <ahmetsafaorhan@gmail.com>
2021-03-26 11:43:19 +0300

    Refactor some code. Make error handling logic more readable and robust.

--- app/src/main/java/com/example/cactusnotes/SignUpActivity.kt
+++ app/src/main/java/com/example/cactusnotes/SignUpActivity.kt
@@ -20,7 +20,6 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class SignUpActivity : AppCompatActivity() {
-
     private lateinit var binding: SignUpActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/cactusnotes/model/CactusModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.cactusnotes.model
-
-data class CactusModel(
-    val email: String,
-    val username: String,
-    val password: String
-)
