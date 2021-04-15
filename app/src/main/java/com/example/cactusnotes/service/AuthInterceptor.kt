401274b6f4d7ce1105633fc9b1003d99c881bbd8
Ahmet Safa Orhan <ahmetsafaorhan@gmail.com>
2021-04-15 11:30:39 +0300

    Make interceptor conditional

--- app/src/main/java/com/example/cactusnotes/service/AuthInterceptor.kt
+++ app/src/main/java/com/example/cactusnotes/service/AuthInterceptor.kt
@@ -7,10 +7,14 @@ import okhttp3.Response
 class AuthInterceptor(private val userStore: UserStore) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val requestBuilder = chain.request().newBuilder()
+
         // If token has been saved, add it to the request
-        userStore.getJwt()?.let {
-            requestBuilder.addHeader("Authorization", "Bearer $it")
+        if (chain.request().url().pathSegments()[0] != "auth") {
+            userStore.getJwt()?.let {
+                requestBuilder.addHeader("Authorization", "Bearer $it")
+            }
         }
+
         return chain.proceed(requestBuilder.build())
     }
 }
